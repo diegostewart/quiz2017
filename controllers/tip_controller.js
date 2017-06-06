57158a8699c9bdf1bedb31263aea959af65a8fb1
diegostewart <diegostewart1995@gmail.com>
2017-06-06 13:41:27 +0200

    p53 final

--- controllers/tip_controller.js
+++ controllers/tip_controller.js
@@ -5,7 +5,11 @@ var Sequelize = require('sequelize');
 // Autoload la pista asociado a :tipId
 exports.load = function (req, res, next, tipId) {
 
-    models.Tip.findById(tipId)
+    models.Tip.findById(tipId,{
+        include: [
+            {model: models.User, as: 'Author'}
+        ]
+    })
     .then(function (tip) {
         if (tip) {
             req.tip = tip;
@@ -36,11 +40,114 @@ exports.new = function (req, res, next) {
 
 // POST /quizzes/:quizId/tips
 exports.create = function (req, res, next) {
-
+    var authorId = req.session.user && req.session.user.id || 0;
     var tip = models.Tip.build(
         {
             text: req.body.text,
-            QuizId: req.quiz.id
+            QuizId: req.quiz.id,
+        AuthorId: authorId
+        });
+
+    tip.save()
+    .then(function (tip) {
+        req.flash('success', 'Pista creado con éxito.');
+
+        res.redirect("back");
+        // res.redirect('/quizzes/' + req.quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
+
+        req.flash('error', 'Errores en el formulario:');
+        for (var i in error.errors) {
+            req.flash('error', "" + error.errors[i].value);
+        }
+
+        // Necesario usar ""+ en la sentencia anterior porque se pierde la referencia al error.
+        res.redirect("back");
+    })
+    .catch(function (error) {
+        req.flash('error', 'Error al crear una Pista: ' + error.message);
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId/tips/:tipId/accept
+exports.accept = function (req, res, next) {
+
+    req.tip.accepted = true;
+
+    req.tip.save(["accepted"])
+    .then(function (tip) {
+        req.flash('success', 'Pista aceptada con éxito.');
+        res.redirect('/quizzes/' + req.params.quizId);
+    })
+    .catch(function (error) {
+        req.flash('error', 'Error al aceptar una Pista: ' + error.message);
+        next(error);
+    });
+};
+
+
+// DELETE /quizzes/:quizId/tips/:tipId
+exports.destroy = function (req, res, next) {
+
+    req.tip.destroy()
+    .then(function () {
+        req.flash('success', 'Pista eliminada con éxito.');
+        res.redirect('/quizzes/' + req.params.quizId);
+    })
+    .catch(function (error) {
+        next(error);
+    });
+};var models = require('../models');
+var Sequelize = require('sequelize');
+
+
+// Autoload la pista asociado a :tipId
+exports.load = function (req, res, next, tipId) {
+
+    models.Tip.findById(tipId,{
+        include: [
+            {model: models.User, as: 'Author'}
+        ]
+    })
+    .then(function (tip) {
+        if (tip) {
+            req.tip = tip;
+            next();
+        } else {
+            next(new Error('No existe tipId=' + tipId));
+        }
+    })
+    .catch(function (error) {
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId/tips/new
+exports.new = function (req, res, next) {
+
+    var tip = {
+        text: ""
+    };
+
+    res.render('tips/new', {
+        tip: tip,
+        quiz: req.quiz
+    });
+};
+
+
+// POST /quizzes/:quizId/tips
+exports.create = function (req, res, next) {
+    var authorId = req.session.user && req.session.user.id || 0;
+    var tip = models.Tip.build(
+        {
+            text: req.body.text,
+            QuizId: req.quiz.id,
+        AuthorId: authorId
         });
 
     tip.save()
